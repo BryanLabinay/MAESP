5132af29de0e4da8868f95eeeddf8852ff3b775d
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-09-12 21:14:24 +0800

    blade for brgy.offices

---
+++ resources/views/admin/brgy-offices.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    @section('css')
+        {{-- Boostrap CDN --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    @stop
+    @section('content_header')
+        <h5 class="fw-semibold text-md">Brgy.Offices</h5>
+        <hr class="mt-0">
+    @stop
+
+    @section('content')
+        <div class="container-fluid">
+
+        </div>
+    @stop
+</x-app-layout>
