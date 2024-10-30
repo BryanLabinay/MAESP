9d07ae7ddb62127de94dfde3f74ba6f250660002
Mark Bryan Labinay <smokefacebook02@gmail.com>
2024-10-30 08:49:52 +0800

    add count display

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -17,14 +17,14 @@
                     <!-- small box -->
                     <div class="small-box bg-info">
                         <div class="inner">
-                            <h3>150</h3>
-
+                            <h3>{{ $countall }}</h3>
                             <p class="fw-bold">Total Barangay</p>
                         </div>
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="{{ route('barangays.index') }}" class="small-box-footer">More info <i
+                                class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
                 <!-- ./col -->
@@ -32,8 +32,8 @@
                     <!-- small box -->
                     <div class="small-box bg-success">
                         <div class="inner">
-                            <h3>53<sup style="font-size: 20px">%</sup></h3>
-
+                            <h3>{{ $farmer }}</h3>
+                            {{-- <sup style="font-size: 20px">%</sup> --}}
                             <p class="fw-bold">Total Farmers</p>
                         </div>
                         <div class="icon">
