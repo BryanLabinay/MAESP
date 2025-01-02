649b0d604e9991c547ae533d5316053ee6ff112f
Arnold Fernando <junfernando727@gmail.com>
2025-01-02 19:40:45 +0800

    refactor service creation and content handling to support multiple image uploads

--- app/Models/ServiceContent.php
+++ app/Models/ServiceContent.php
@@ -4,13 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use PhpParser\Node\Expr\Cast;
 
 class ServiceContent extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['service_id', 'header','image', 'content'];
+    protected $fillable = ['service_id', 'header', 'image', 'content'];
 
+    protected $casts = [
+        'image' => 'array',
+    ];
     public function service()
     {
         return $this->belongsTo(Service::class);
