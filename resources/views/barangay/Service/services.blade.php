71c3fc5f52ef1972fb7d6ebfa62eae8dba84931e
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-03 21:18:03 +0800

    refactor service view to improve layout and enhance image handling

--- resources/views/barangay/Service/services.blade.php
+++ resources/views/barangay/Service/services.blade.php
@@ -5,32 +5,31 @@
     <hr class="mt-0">
     <div class="container-fluid">
 
-        <p><strong>Service Name:</strong> {{ $service->service_name }}</p>
+        <p><strong>{{ $service->service_name }}</strong></p>
 
-        @if($service->image)
+        @if ($service->image)
             <p><strong>Image:</strong></p>
             <img src="{{ asset('storage/' . $service->image) }}" alt="{{ $service->service_name }}" class="img-fluid">
         @else
-            <p><strong>No image available for this service.</strong></p>
+            <p><strong></strong></p>
         @endif
 
         <h5 class="mt-4">Content for this Service</h5>
 
-        @if($serviceContents->isEmpty())
+        @if ($serviceContents->isEmpty())
             <p>No content yet added.</p>
         @else
             @foreach ($serviceContents as $content)
                 <div class="content-item">
-                    @if($content->header)
+                    @if ($content->header)
                         <h6><strong>{{ $content->header }}</strong></h6>
                     @endif
                     <p>{{ $content->content }}</p>
 
-                    @if($content->image)
-                    <img src="{{ asset($content->image) }}" alt="Content Image" class="img-fluid mb-3">
-                @else
-
-                    <p><strong>No image available for this content.</strong></p>
+                    @if ($content->image)
+                        <img src="{{ asset($content->image) }}" alt="Content Image" class="img-fluid mb-3">
+                    @else
+                        <p><strong>No image available for this content.</strong></p>
                     @endif
                 </div>
             @endforeach
