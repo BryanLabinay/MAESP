c054f9310b1cf087deac059f13ff77fd39f26001
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-04 18:54:35 +0800

    add Transparency index view with Bootstrap and Poppins font

---
+++ resources/views/admin/Transparency/index.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    @section('css')
+        {{-- Boostrap CDN --}}
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+        {{-- Google Fonts --}}
+        <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+            rel="stylesheet">
+        <style>
+            body {
+                font-family: 'Poppins';
+            }
+        </style>
+    @stop
+
+    @section('content_header')
+        <h5 class="fw-semibold text-md">Transparency</h5>
+        <hr class="mt-0">
+    @stop
+
+    @section('content')
+        <div class="container-fluid">
+
+        </div>
+    @stop
+</x-app-layout>
