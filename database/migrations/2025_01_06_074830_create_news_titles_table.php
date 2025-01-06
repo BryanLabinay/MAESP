7a620a00aa69a347c01d0338bf626c672956d4a9
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-06 17:54:31 +0800

    add migration for news_titles table with relationships

---
+++ database/migrations/2025_01_06_074830_create_news_titles_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('news_titles', function (Blueprint $table) {
+            $table->id();
+            $table->string('news_name')->nullable();
+            $table->string('description')->nullable();
+            $table->text('image')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('news_titles');
+    }
+};
