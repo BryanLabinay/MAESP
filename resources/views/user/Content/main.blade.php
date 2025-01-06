90755bf6594420375ab78b5afc36ed1c5d43c625
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-06 17:59:52 +0800

    add main layout for user view with hero, about, recent posts, and call to action sections

---
+++ resources/views/user/Content/main.blade.php
@@ -0,0 +1,262 @@
+<x-user-layout>
+
+    <!-- Hero Section -->
+    <section id="hero" class="hero section dark-background">
+
+        <div id="hero-carousel" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="5000">
+
+            <div class="carousel-item active">
+                <img src="{{ asset('assets/img/bg.jpg') }}" alt="">
+                <div class="carousel-container">
+                    <h2 class="text-center text-uppercase">Municipal Agriculture Service Portal - Aparri</h2>
+                </div>
+            </div><!-- End Carousel Item -->
+
+            <div class="carousel-item">
+                <img src="assets/img/hero_2.jpg" alt="">
+                <div class="carousel-container">
+                    {{-- <h2>Organic vegetables is good for health</h2>
+             <p>Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id
+                 quod maxime placeat facere possimus.</p> --}}
+                </div>
+            </div><!-- End Carousel Item -->
+
+            <div class="carousel-item">
+                <img src="assets/img/hero_3.jpg" alt="">
+                <div class="carousel-container">
+                    {{-- <h2>Providing Fresh Produce Every Single Day</h2>
+             <p>Beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur
+                 aut odit aut fugit.</p> --}}
+                </div>
+            </div><!-- End Carousel Item -->
+
+            <div class="carousel-item">
+                <img src="assets/img/hero_4.jpg" alt="">
+                <div class="carousel-container">
+                    {{-- <h2>Farming as a Passione</h2>
+             <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+                 sed quia non numquam eius.</p> --}}
+                </div>
+            </div><!-- End Carousel Item -->
+
+            <div class="carousel-item">
+                <img src="assets/img/hero_5.jpg" alt="">
+                <div class="carousel-container">
+                    {{-- <h2>Good Food For All</h2>
+             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                 labore et dolore magna aliqua.</p> --}}
+                </div>
+            </div><!-- End Carousel Item -->
+
+            <a class="carousel-control-prev" href="#hero-carousel" role="button" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
+            </a>
+
+            <a class="carousel-control-next" href="#hero-carousel" role="button" data-bs-slide="next">
+                <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
+            </a>
+
+            <ol class="carousel-indicators"></ol>
+
+        </div>
+
+    </section>
+    <!-- /Hero Section -->
+
+    <!-- About Section -->
+    <section id="about" class="about section">
+
+        <div class="content">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-6 mb-4 mb-lg-0">
+                        <img src="assets/img/img_long_5.jpg" alt="Image " class="img-fluid img-overlap"
+                            data-aos="zoom-out">
+                    </div>
+                    <div class="col-lg-5 ml-auto" data-aos="fade-up" data-aos-delay="100">
+                        <h3 class="content-subtitle text-white opacity-50">Why Choose</h3>
+                        <h2 class="content-title mb-4">
+                            Municipal<strong>Agriculture</strong> Service Portal
+                        </h2>
+                        <p class="opacity-50">
+                            Reprehenderit, odio laboriosam? Blanditiis quae ullam quasi illum
+                            minima nostrum perspiciatis error consequatur sit nulla.
+                        </p>
+
+                        <div class="row my-5">
+                            <div class="col-lg-12 d-flex align-items-start mb-4">
+                                <i class="bi bi-cloud-rain me-4 display-6"></i>
+                                <div>
+                                    <h4 class="m-0 h5 text-white">Plants needs rain</h4>
+                                    <p class="text-white opacity-50">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 d-flex align-items-start mb-4">
+                                <i class="bi bi-heart me-4 display-6"></i>
+                                <div>
+                                    <h4 class="m-0 h5 text-white">Love organic foods</h4>
+                                    <p class="text-white opacity-50">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                            <div class="col-lg-12 d-flex align-items-start">
+                                <i class="bi bi-shop me-4 display-6"></i>
+                                <div>
+                                    <h4 class="m-0 h5 text-white">Sell vegies</h4>
+                                    <p class="text-white opacity-50">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- /About Section -->
+
+    <!-- Recent Posts Section -->
+    <section id="recent-posts" class="recent-posts section">
+
+        <!-- Section Title -->
+        <div class="container section-title" data-aos="fade-up">
+            <h2 class="text-uppercase">Agriculture Office Team in Action</h2>
+            {{-- <p>Necessitatibus eius consequatur</p> --}}
+        </div><!-- End Section Title -->
+
+        <div class="container">
+
+            <div class="row gy-5">
+
+                <div class="col-xl-4 col-md-6">
+                    <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="100">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img/blog/blog-1.jpg" class="img-fluid" alt="">
+                            <span class="post-date">December 12</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Eum ad dolor et. Autem aut fugiat debitis</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-person"></i> <span class="ps-2">Julia Parker</span>
+                                </div>
+                                <span class="px-3 text-black-50">/</span>
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-folder2"></i> <span class="ps-2">Politics</span>
+                                </div>
+                            </div>
+
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i
+                                    class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+
+                <div class="col-xl-4 col-md-6">
+                    <div class="post-item position-relative h-100" data-aos="fade-up" data-aos-delay="200">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img/blog/blog-2.jpg" class="img-fluid" alt="">
+                            <span class="post-date">July 17</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Et repellendus molestiae qui est sed omnis</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-person"></i> <span class="ps-2">Mario Douglas</span>
+                                </div>
+                                <span class="px-3 text-black-50">/</span>
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-folder2"></i> <span class="ps-2">Sports</span>
+                                </div>
+                            </div>
+
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i
+                                    class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+
+                <div class="col-xl-4 col-md-6" data-aos="fade-up" data-aos-delay="300">
+                    <div class="post-item position-relative h-100">
+
+                        <div class="post-img position-relative overflow-hidden">
+                            <img src="assets/img/blog/blog-3.jpg" class="img-fluid" alt="">
+                            <span class="post-date">September 05</span>
+                        </div>
+
+                        <div class="post-content d-flex flex-column">
+
+                            <h3 class="post-title">Quia assumenda est et veritati tirana ploder</h3>
+
+                            <div class="meta d-flex align-items-center">
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-person"></i> <span class="ps-2">Lisa Hunter</span>
+                                </div>
+                                <span class="px-3 text-black-50">/</span>
+                                <div class="d-flex align-items-center">
+                                    <i class="bi bi-folder2"></i> <span class="ps-2">Economics</span>
+                                </div>
+                            </div>
+
+                            <hr>
+
+                            <a href="blog-details.html" class="readmore stretched-link"><span>Read More</span><i
+                                    class="bi bi-arrow-right"></i></a>
+
+                        </div>
+
+                    </div>
+                </div><!-- End post item -->
+
+            </div>
+
+        </div>
+
+    </section><!-- /Recent Posts Section -->
+
+    <!-- Call To Action Section -->
+    <section id="call-to-action" class="call-to-action section light-background">
+
+        <div class="content">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <h3 class="text-uppercase">Feedback</h3>
+                        <p class="opacity-50">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Nesciunt, reprehenderit!
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <form action="forms/newsletter.php" class="form-subscribe php-email-form">
+                            <div class="form-group d-flex align-items-stretch">
+                                <input type="email" name="email" class="form-control h-100"
+                                    placeholder="Enter your e-mail">
+                                <input type="submit" class="btn btn-secondary px-4" value="Subcribe">
+                            </div>
+                            <div class="loading">Loading</div>
+                            <div class="error-message"></div>
+                            <div class="sent-message">
+                                Your subscription request has been sent. Thank you!
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- /Call To Action Section -->
+
+</x-user-layout>
