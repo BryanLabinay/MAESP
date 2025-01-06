942c8075f5618aba3e83b15e692e70e11298d255
Mark Bryan Labinay <smokefacebook02@gmail.com>
2025-01-06 17:58:21 +0800

    refactor navigation layout to use Laravel routing and improve structure with dynamic news listing

--- resources/views/layouts/welcome/navigation.blade.php
+++ resources/views/layouts/welcome/navigation.blade.php
@@ -1,17 +1,17 @@
-<header id="header" class="header d-flex align-items-center position-sticky top-0 p-1 body" style="z-index: 1000;">
+<header id="header" class="header d-flex align-items-center position-sticky top-0 p-1 body shadow-sm"
+    style="z-index: 1000;">
     <div class="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
         <a href="index.html" class="d-flex align-items-center mt-1">
             <!-- Image logo on the left -->
-            <img src="assets/img/masp-logo.jpg" alt="AgriCulture" style="width: 60px; height: 60px;">
+            <img src="{{ asset('assets/img/masp-logo.jpg') }}" alt="AgriCulture" style="width: 60px; height: 60px;">
             <h3 class="mt-2 fw-bold text-success">MASP</h3>
         </a>
 
         <nav id="navmenu" class="navmenu">
             <ul>
-                <li><a href="#index.html" class="active">Home</a></li>
-                <li><a href="#about.html">About Us</a></li>
-                {{-- <li><a href="#about.html">Services</a></li> --}}
-                {{-- <li><a href="services.html">Our Services</a></li> --}}
+                <li><a href="{{ url('/') }}" class="active">Home</a></li>
+                <li><a href="{{ route('show.about') }}">About Us</a></li>
+                {{-- SERVICES --}}
                 <li class="dropdown"><a href="#"><span>Services</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
@@ -30,7 +30,25 @@
                     </ul>
                 </li>
 
+                {{-- News and Updates --}}
+                <li class="dropdown"><a href="#"><span>News and Update</span> <i
+                            class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                        @php
+                            $news = \App\Models\NewsTitle::all();
+                        @endphp
+                        @if ($news->isEmpty())
+                            <li><a href="#">No services available</a></li>
+                        @else
+                            @foreach ($news as $new)
+                                <li><a href="{{ route('user.news', ['id' => $new->id]) }}">{{ $new->news_name }}</a>
+                                </li>
+                            @endforeach
+                        @endif
+                    </ul>
+                </li>
 
+                {{-- MEDIA RESOURCES --}}
                 <li class="dropdown"><a href="#"><span>Media Resources</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
@@ -49,11 +67,10 @@
                     </ul>
                 </li>
 
+                {{-- PORTFOLIO --}}
+                <li><a href="{{ route('show.portfolio') }}">Portfolio</a></li>
 
-
-
-                <li><a href="blog.html">Portfolio</a></li>
-
+                {{-- TRANSPARENCY --}}
                 <li class="dropdown"><a href="#"><span>Transparency</span> <i
                             class="bi bi-chevron-down toggle-dropdown"></i></a>
                     <ul>
@@ -72,7 +89,10 @@
                     </ul>
                 </li>
 
-                <li><a href="contact.html">Contact</a></li>
+                {{-- CONTACT --}}
+                <li><a href="{{ route('show.contact') }}">Contact</a></li>
+
+
                 @if (Route::has('login'))
                     @auth
                         <a href="{{ url('/home') }}"
